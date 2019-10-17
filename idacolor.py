b2cff456691d43ea7d64ac7f53acecac48124af1
Eric Merritt <ericj.merritt@gmail.com>
2019-10-17 15:21:04 -0500

    Update idacolor.py
    Updated IDA script for python 3.7 and IDA 7.4

--- idacolor.py
+++ idacolor.py
@@ -1,39 +1,43 @@
+import idc
+
 currentEA = ''
 currentMnem = ''
 prevMnem = ''
 nextMnem = ''
 currentOp = ''
 
-print("Running IDA setup script")
 
+print("Running IDA setup script")
 print("Setting startup options")
+
 # set demangled names options to show names
-SetCharPrm(INF_DEMNAMES, DEMNAM_NAME)
+idc.set_inf_attr(INF_DEMNAMES, DEMNAM_NAME)
 # show the address next to each command
-# SetCharPrm(INF_SHOWPREF, 1)
+idc.set_inf_attr(INF_OUTFLAGS, OFLG_SHOW_PREF)
+idc.set_inf_attr(INF_PREFFLAG, 0)
 print("Finished setting options")
 
-currentEA = FirstSeg()
-currentEA = NextHead(currentEA, 0xFFFFFFFFFFFFFFFF)
+currentEA = idc.get_first_seg()
+currentEA = idc.next_head(currentEA, 0xFFFFFFFFFFFFFFFF)
 
 while(currentEA != BADADDR):
-	currentMnem = GetMnem(currentEA)
+	currentMnem = 	idc.print_insn_mnem(currentEA)
 
 	#Highlight call functions
 	if(currentMnem == "call"):
 	#check to see if it's a call pop
-		nextMnem = GetMnem(GetOperandValue(currentEA, 0))
+		nextMnem = 	idc.print_insn_mnem(idc.get_operand_value(currentEA, 0))
 		if nextMnem == "pop":
-			SetColor(currentEA, CIC_ITEM, 0xff1d4a)
-			SetColor(GetOperandValue(currentEA, 0), CIC_ITEM, 0x4a1dFF)
+			idc.set_color(currentEA, CIC_ITEM, 0xff1d4a)
+			idc.set_color(idc.get_operand_value(currentEA, 0), CIC_ITEM, 0x4a1dFF)
 		else:
-			SetColor(currentEA, CIC_ITEM, 0xc7c7ff)
+			idc.set_color(currentEA, CIC_ITEM, 0xc7c7ff)
         
     #Non-zeroing XORs are often signs of data encoding
 	if (currentMnem == "xor"):
-		if (GetOpnd(currentEA, 0) != GetOpnd(currentEA, 1)): 
-			SetColor(currentEA, CIC_ITEM, 0xFFFF00)
+		if (idc.print_operand(currentEA, 0) != idc.print_operand(currentEA, 1)): 
+			idc.set_color(currentEA, CIC_ITEM, 0xFFFF00)
         
         
-	currentEA = NextHead(currentEA, 0xFFFFFFFFFFFFFFFF)	
+	currentEA = idc.next_head(currentEA, 0xFFFFFFFFFFFFFFFF)	
 	prevMnem = currentMnem
